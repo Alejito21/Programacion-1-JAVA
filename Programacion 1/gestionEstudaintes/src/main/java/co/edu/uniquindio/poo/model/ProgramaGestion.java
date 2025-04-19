15dc8b8beff230b4c23b0d4cdcde9735ad21cf17
Alejito <alejo2006rivera@gmail.com>
2025-04-18 22:51:48 -0500

    ProgramaGestion

--- Programacion 1/gestionEstudaintes/src/main/java/co/edu/uniquindio/poo/model/ProgramaGestion.java
+++ Programacion 1/gestionEstudaintes/src/main/java/co/edu/uniquindio/poo/model/ProgramaGestion.java
@@ -1,6 +1,7 @@
 package co.edu.uniquindio.poo.model;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class ProgramaGestion {
     private String nombre;
@@ -11,6 +12,33 @@ public class ProgramaGestion {
         this.listaEstudiantes = new ArrayList<>();
     }
 
+    public String registrarEstudiante(Estudiante estudiante) {
+        String mensaje="";
+        Optional<Estudiante> encontrado = buscarEstudiante(estudiante);
+        if (encontrado.isPresent()) {
+            mensaje="Estudiante ya registrado";
+        }else{
+            listaEstudiantes.add(estudiante);
+            mensaje="Estudiante registrado";
+        }
+        return mensaje;
+    }
+
+    public Optional<Estudiante> buscarEstudiante(Estudiante estudiante) {
+        return listaEstudiantes.stream().filter(e->e.getId().equals(estudiante.getId())).findFirst();
+    }
+
+    public String eliminarEstudiante(Estudiante estudiante) {
+        String mensaje="";
+        Optional<Estudiante> encontrado = buscarEstudiante(estudiante);
+        if (encontrado.isPresent()) {
+            listaEstudiantes.remove(estudiante);
+            mensaje="Estudiante eliminado";
+        }else{
+            mensaje="Estudiante no existe";
+        }
+        return mensaje;
+    }
 
     public String getNombre() {
         return nombre;
